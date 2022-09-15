f38c5c831685c2130f56ba7bdcdfecb498fd6471
Tazmah <abdulmuizzayo6@gmail.com>
2022-09-15 12:44:01 -0700

    1st

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,17 @@
 /**
  * _isupper - function that verifies if a character is uppercase or not
  * @c: tested character
- * Return: returns 1 if it is uppercase , 0 if not
+ * Return: returns 1 if it is a uppercase, 0 if not
  */
 
 int _isupper(int c)
 {
-	return (c >= "A" && c <="Z");
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
