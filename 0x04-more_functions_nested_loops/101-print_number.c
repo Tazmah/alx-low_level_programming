7439875584cf90b9b5f642e5b0540bc5a73db480
Tazmah <abdulmuizzayo6@gmail.com>
2022-09-15 15:39:40 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: int type number
+ * Description: can oly use _putchar to print
+ */
+void print_number(int n)
+{
+	printf ("%d", n);
+}
