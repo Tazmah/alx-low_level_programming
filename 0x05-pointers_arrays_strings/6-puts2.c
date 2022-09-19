8337307dd67a623dcaaf955456f92536d3098ade
Tazmah <abdulmuizzayo6@gmail.com>
2022-09-19 06:07:49 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,12 @@
 
 void puts2(char *str)
 {
-  int i;
+	int i;
 
-  for (i = 0; str[i] != '\0'; ++i)
-    {
-      if (i % 2 == 0)
-	putchar(str[i]);
-    }
-  putchar('\n');
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+		putchar(str[i]);
+	}
+	  putchar('\n');
 }
