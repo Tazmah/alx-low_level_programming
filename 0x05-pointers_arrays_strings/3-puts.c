7ff01649f560b194c6205d63ee9a20f9e5cf8236
Tazmah <abdulmuizzayo6@gmail.com>
2022-09-19 04:39:47 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string to stdout
+ * @str: the parameter
+ * Return: none since void
+ */
+void _puts(char *str)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}
