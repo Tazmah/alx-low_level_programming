472695fc989e39ac9384a9a864bb2f2ea5698e65
Tazmah <abdulmuizzayo6@gmail.com>
2022-09-26 07:43:34 -0700

    happy

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,17 +8,17 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-  int i, j;
+	int i, j;
 
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      for (j = 0; accept[j] != '\0'; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	  if (s[i] == accept[j])
-	    {
-	      return (&s[i]);
-	    }
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
 	}
-    }
-  return (0);
+	return (0);
 }
