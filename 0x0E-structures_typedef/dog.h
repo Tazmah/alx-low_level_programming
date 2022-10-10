ca6008d622e22231ba020f453d12f09bdee9f9b8
Tazmah <abdulmuizzayo6@gmail.com>
2022-10-10 05:36:33 -0700

    updated the dod.h file with #define and end

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,24 @@
-#include <stdio.h>
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: oner of the dog
+ *
+ * Description: struct called that stores the above param
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+#endif
