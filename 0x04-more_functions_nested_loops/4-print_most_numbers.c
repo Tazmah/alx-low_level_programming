10e51c2d8adf065b5402677c395a1a5185ddc5fd
Tazmah <abdulmuizzayo6@gmail.com>
2022-10-22 05:06:44 -0700

    updated the 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,10 +5,14 @@
  */
 void print_most_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
-		if (c != '2' && c != '4')
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != 2 && c != 4)
+		{
 			_putchar(c);
+		}
+	}
 	_putchar('\n');
 }
