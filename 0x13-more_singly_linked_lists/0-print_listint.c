689a9fee35551d019eae2649f68b48c8d306b7d0
Tazmah <abdulmuizzayo6@gmail.com>
2022-12-12 12:15:56 +0100

    new

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the element of a listint n
+ * @h: value or memory of the first node
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+	while (h != NULL)
+		h = h->next;
+		i++;
+	return (i);
+}
